f91b8b45a0263925939ed9f36fa14628bb78bb1e
vitorsantos30 <vitormarlondossantoscorrea@gmail.com>
2025-01-30 22:18:19 +0000

    cadastro dos estados

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,29 @@
 //Teste larissa
 
 int main() {
+
+    printf("Desafio Super Trunfo - Países\n");
+
+    printf("Brasil\n");
+
+    char estadoA[50] = "Minas Gerais\n";
+    char estadoB[50] = "Sao Paulo\n";
+    char estadoC[50] = "Rio de Janeiro\n";
+    char estadoD[50] = "Goias\n";
+    char estadoE[50] = "Espirito Santo\n";
+    char estadoF[50] = "Bahia\n";
+    char estadoG[50] = "Rio Grande do Sul\n";
+    char estadoH[50] = "Alagoas\n";
+
+        printf("Estado: %s\n", estadoA);
+        printf("Estado: %s\n", estadoB);
+        printf("Estado: %s\n", estadoC);
+        printf("Estado: %s\n", estadoD);
+        printf("Estado: %s\n", estadoE);
+        printf("Estado: %s\n", estadoF);
+        printf("Estado: %s\n", estadoG);
+        printf("Estado: %s\n", estadoH);
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
